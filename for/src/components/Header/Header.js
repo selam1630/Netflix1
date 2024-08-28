8911e50242e0d95809593dbea5bce7cb55de4131
selam1630 <fikruselam29@gmail.com>
2024-08-28 15:28:55 -0700

    header and footer

---
+++ for/src/components/Header/Header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './header.css';
+import Netflix_Logo_RGB from '../../images/Netflix_Logo_RGB.png';
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+
+const Header = () => {
+  return (
+    <div className='header_outer_container'>
+      <div className='header_container'>
+        <div className='header_left'>
+          <ul className='for'>
+            <li><img src={Netflix_Logo_RGB} alt='Netflix Logo' width="100" /></li>
+            <li>Netflix</li>
+            <li>Home</li>
+            <li>TV Show</li>
+            <li>Movies</li>
+            <li>Latest</li>
+            <li>My List</li>
+            <li>Browse by Languages</li>
+          </ul>
+        </div>
+        <div className='header_right'>
+          <ul className='right-menu'>
+            <li><SearchIcon /></li>
+            <li><NotificationsIcon /></li>
+            <li><AccountBoxIcon /></li>
+            <li><ArrowDropDownIcon/></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
